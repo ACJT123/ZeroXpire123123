ca99d72b876c07890f520af99eec263ef371e5af
acjta <chengjt-wp20@student.tarc.edu.my>
2023-07-08 15:33:28 +0800

    Commit testing 2

--- app/src/main/java/my/edu/tarc/zeroxpire/RecipeFragment.kt
+++ app/src/main/java/my/edu/tarc/zeroxpire/RecipeFragment.kt
@@ -16,6 +16,13 @@ private const val ARG_PARAM2 = "param2"
  * Use the [RecipeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+class RecipeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var parama: String? = null
+    private var parambb: String? = null
+    private var paramccc: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
